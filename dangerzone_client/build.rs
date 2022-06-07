10462c18a03df2f346b8fdad44b2f083a4163680
Yves Zoundi <yveszoundi@gmail.com>
2022-06-06 21:50:15 -0400

    Bump version and upgrade to fltk-1.3.8

--- dangerzone_client/build.rs
+++ dangerzone_client/build.rs
@@ -3,6 +3,16 @@ fn main() {
         use winres;
         let mut res = winres::WindowsResource::new();
         res.set_icon("images/Dangerzone_icon.ico");
+
+        if cfg!(unix) {
+            // paths for X64 on archlinux
+            res.set_toolkit_path("/usr/x86_64-w64-mingw32/bin");
+            // ar tool for mingw in toolkit path
+            res.set_ar_path("ar");
+            // windres tool
+            res.set_windres_path("/usr/bin/x86_64-w64-mingw32-windres");
+        }
+        
         res.compile().unwrap();
     }
 }

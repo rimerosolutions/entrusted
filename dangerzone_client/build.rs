3dd9ac6326151c67be8f74335354fc422f16c21a
Yves Zoundi <yveszoundi@gmail.com>
2022-06-09 14:16:50 -0400

    [dangerzone_client] Add properly icons for Windows executables
    Cargo has some annoying limitations as you apparently can't specify dependencies per "target" platform
    - In your case, we're compiling Windows binaries from Linux insider a container image
    - Previously all the code was essentially always skipped in the build.rs file, as the build machine is not Windows...

--- dangerzone_client/build.rs
+++ dangerzone_client/build.rs
@@ -1,5 +1,7 @@
 fn main() {
-    #[cfg(target_os = "windows")] {
-        embed_resource::compile("icon.rc");
+    if let Ok(target_sys) = std::env::var("CARGO_CFG_TARGET_OS") {
+        if target_sys == "windows" {
+            embed_resource::compile("icon.rc");
+        }
     }
 }

9b88b147175ebcebe7ec82ecb86a43deda0aa7a5
Yves Zoundi <yveszoundi@gmail.com>
2022-06-12 23:34:28 -0400

    [dangerzone_httpserver] Output file name discrepancy

--- dangerzone_httpserver/src/main.rs
+++ dangerzone_httpserver/src/main.rs
@@ -301,7 +301,7 @@ async fn download(info: actix_web::web::Path<String>, req: HttpRequest, l10n: Da
     println!("{}: {}", l10n_ref.get_message("msg-download-from"), req.uri());
 
     let file_id = info.into_inner();
-    let filename = [&file_id, "-dgz.pdf"].concat();
+    let filename = [&file_id, "-", config::DEFAULT_FILE_SUFFIX, ".pdf"].concat();
     let filepath = env::temp_dir().join(config::PROGRAM_GROUP).join(filename.clone());
     let filepath_buf = PathBuf::from(filepath);
 
@@ -390,7 +390,7 @@ async fn upload(req: HttpRequest, payload: Multipart, ci_image_name: Data<Mutex<
             };
             let input_path = PathBuf::from(new_upload_info.2);
             let output_path =
-                PathBuf::from(tmpdir).join([request_id.clone(), "-".to_string(), config::DEFAULT_FILE_SUFFIX.to_string()].concat());
+                PathBuf::from(tmpdir).join([request_id.clone(), "-".to_string(), config::DEFAULT_FILE_SUFFIX.to_string(), ".pdf".to_string()].concat());
 
             let container_image_name = ci_image_name.lock().unwrap().to_string();
             if let Err(ex) = run_dangerzone(request_id, container_image_name, input_path, output_path, ocr_lang_opt, l10n_async_ref.clone(), langid_ref).await {

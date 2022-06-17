c56055d39ee56df5b50158268322040d2d0c1dec
Yves Zoundi <yveszoundi@gmail.com>
2022-06-17 12:31:16 -0400

    [dangerzone_client] build.rs consistency

--- dangerzone_client/build.rs
+++ dangerzone_client/build.rs
@@ -5,10 +5,8 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=translations");
-
-    let dir = Path::new("translations");
     
-    for entry in fs::read_dir(dir)? {
+    for entry in fs::read_dir(Path::new("translations"))? {
         let path = entry?.path();
         let input = path.join("LC_MESSAGES").join("messages.po");
 
@@ -16,23 +14,18 @@ fn main() -> Result<(), Box<dyn Error>> {
             println!("cargo:info={}", format!("Processing translation PO file: {}", &input.display()));
                 
             let output = path.join("LC_MESSAGES").join("messages.mo");
-            let catalog = po_file::parse(Path::new(&input));
+            let catalog_ret = po_file::parse(Path::new(&input));
 
-            if let Err(ex) = catalog {
-                return Err(format!("Failed to parse PO file {}.\n{}", &input.display(), ex.to_string()).into());
+            match catalog_ret {
+                Ok(catalog) => {                    
+                    if let Err(ex) = mo_file::write(&catalog, Path::new(&output)) {
+                        return Err(format!("Failed to compile MO file for {}.\n{}", &input.display(), ex.to_string()).into());
+                    }                    
+                },
+                Err(ex) => {
+                    return Err(format!("Failed to parse PO file {}.\n{}", &input.display(), ex.to_string()).into());
+                }
             }
-
-            let catalogc = catalog.unwrap();
-
-            if let Err(ex) = mo_file::write(&catalogc, Path::new(&output)) {
-                return Err(format!("Failed to compile MO file for {}.\n{}", &input.display(), ex.to_string()).into());
-            }
-        }
-    }
-
-    if let Ok(target_sys) = std::env::var("CARGO_CFG_TARGET_OS") {
-        if target_sys == "windows" {
-            embed_resource::compile("icon.rc");
         }
     }
     

1ec33a6d9a0f6c0f5d20c589b19bb188fd03e37a
Yves Zoundi <yves_zoundi@hotmail.com>
2022-12-06 15:16:43 -0500

    [entrusted_webserver] Update file upload limit
    The file upload limit did not match anticipated changelog

--- app/entrusted_webserver/src/server.rs
+++ app/entrusted_webserver/src/server.rs
@@ -78,7 +78,7 @@ pub async fn serve(
         .fallback(notfound)
         .layer(DefaultBodyLimit::disable())
         .layer(RequestBodyLimitLayer::new(
-            1000 * 1024 * 1024, /* 1gb, for now... */
+            500 * 1024 * 1024, /* roughly 500 MB limit for file uploads, for now... */
         ))
         .layer(CorsLayer::permissive())
         .layer(Extension(state_ci_image))

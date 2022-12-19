c77e78050a1514a9c437a2ee72398ed31831a940
Yves Zoundi <yves_zoundi@hotmail.com>
2022-12-18 23:04:09 -0500

    Update Dockerfile and script
    - Remove LibreOffice calc executable from the Docker image
    - Simplify helper script to build multi-arch image

--- ci_cd/create_container_image.sh
+++ ci_cd/create_container_image.sh
@@ -13,45 +13,23 @@ rm -rf  ${PROJECTDIR}/entrusted_client/target    \
         ${PROJECTDIR}/entrusted_container/target \
         ${PROJECTDIR}/entrusted_l10n/target
 
-podman rmi --force docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-amd64
-podman rmi --force docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-arm64
 podman rmi --force docker.io/uycyjnzgntrn/entrusted_container:latest
 podman rmi --force docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}
-
-podman rm --force docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-amd64
-podman rm --force docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-arm64
-podman rm --force docker.io/uycyjnzgntrn/entrusted_container:latest
-podman rm --force docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}
-
-podman manifest rm docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-amd64
-podman manifest rm docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-arm64
 podman manifest rm docker.io/uycyjnzgntrn/entrusted_container:latest
 podman manifest rm docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}
 
-buildah manifest create docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}
-
 cd "${PROJECTDIR}"
 
-buildah bud --squash --platform=linux/amd64 --format docker -t docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-amd64 -f entrusted_container/Dockerfile .
+podman build --force-rm --log-level info --squash-all --format docker  --platform linux/arm64/v8 --platform linux/amd64 --manifest docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION} -f entrusted_container/Dockerfile .
+
 retVal=$?
 if [ $retVal -ne 0 ]; then
-    echo "Failure to create entrusted_container container image for amd64"
+    echo "Failure to create entrusted_container container image"
     exit 1
 fi
-buildah manifest add docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION} docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-amd64
-
-
-buildah bud --squash --platform=linux/arm64 --format docker -t docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-arm64 -f entrusted_container/Dockerfile .
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    echo "Failure to create entrusted_container container image for arm64"
-    exit 1
-fi
-buildah manifest add docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION} docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION}-arm64
 
 podman tag docker.io/uycyjnzgntrn/entrusted_container:${APPVERSION} docker.io/uycyjnzgntrn/entrusted_container:latest
 
 podman image prune -f --filter label=stage=entrusted_container_builder
-
 
 cd "${OLD_DIR}"

9496096178664d359dda233f80d17ae75ba9495e
Yves Zoundi <yves_zoundi@hotmail.com>
2023-09-09 01:12:33 -0400

    Disable local macos build pipeline from container image
    - There are some build issues for the GUI with the rust-linux-darwin-builder base container image (arm64 only it seems)
    - Undefined symbols for architecture arm64 _objc_msgSend$sharedPrintInfo and multiple other symbols...

--- ci_cd/macos/build.sh
+++ ci_cd/macos/build.sh
@@ -8,100 +8,107 @@ APPVERSION=$(grep "^version" ${PROJECTDIR}/entrusted_client/Cargo.toml  | cut -d
 CPU_ARCHS="amd64 aarch64"
 RUST_CI_VERSION="1.72.0"
 
-for CPU_ARCH in $CPU_ARCHS ; do
-    ARTIFACTSDIR="${PROJECTDIR}/../artifacts/entrusted-${APPVERSION}-macos-${CPU_ARCH}"
-    rm -rf ${ARTIFACTSDIR}
-done
+echo "The macos local build pipeline is currently broken (GUI build). Cross-compilation from Linux doesn't work, but it compiles just fine on a Mac OS machine."
 
-for CPU_ARCH in $CPU_ARCHS ; do
-    ARTIFACTSDIR="${PROJECTDIR}/../artifacts/entrusted-${APPVERSION}-macos-${CPU_ARCH}"
-    RUST_TARGET="x86_64-apple-darwin"
-    BUILD_PREAMBLE="true"
-    ADDITIONAL_PARAMS=""
-    RUSTFLAGS_PARAMS="RUSTFLAGS='-C target-feature=+crt-static'"
-    EXPORT_PARAMS="export CARGO_NET_GIT_FETCH_WITH_CLI=true; export CARGO_NET_RETRY=10; export CXX=/usr/local/osxcross/target/bin/o64-clang++; export CC=/usr/local/osxcross/target/bin/o64-clang;"
-    STRIP_COMMAND="x86_64-apple-darwin22.4-strip"
+# for CPU_ARCH in $CPU_ARCHS ; do
+#     ARTIFACTSDIR="${PROJECTDIR}/../artifacts/entrusted-${APPVERSION}-macos-${CPU_ARCH}"
+#     rm -rf ${ARTIFACTSDIR}
+# done
 
-    if [ ${CPU_ARCH} != "amd64" ]
-    then
-        RUST_TARGET="aarch64-apple-darwin"
-        EXPORT_PARAMS="export CARGO_NET_GIT_FETCH_WITH_CLI=true; export CARGO_NET_RETRY=10; export CC=oa64-clang; export CXX=oa64-clang++;"    
-        ADDITIONAL_PARAMS="CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=arm64-apple-darwin22.4-clang LIBZ_SYS_STATIC=1"
-        BUILD_PREAMBLE="rustup target add aarch64-apple-darwin"
-        STRIP_COMMAND="arm64-apple-darwin22.4-strip"
-    fi
+# for CPU_ARCH in $CPU_ARCHS ; do
+#     ARTIFACTSDIR="${PROJECTDIR}/../artifacts/entrusted-${APPVERSION}-macos-${CPU_ARCH}"
+#     RUST_TARGET="x86_64-apple-darwin"
+#     ADDITIONAL_PARAMS=""
+#     RUSTFLAGS_PARAMS="RUSTFLAGS='-C target-feature=+crt-static'"
+#     EXPORT_PARAMS="export CARGO_NET_GIT_FETCH_WITH_CLI=true; export CARGO_NET_RETRY=10; export CC=x86_64-apple-darwin22.4-clang; export CXX=x86_64-apple-darwin22.4-clang++;"
+#     STRIP_COMMAND="x86_64-apple-darwin22.4-strip"
 
-    mkdir -p ${ARTIFACTSDIR}
+#     if [ ${CPU_ARCH} != "amd64" ]
+#     then
+#         RUST_TARGET="aarch64-apple-darwin"
+#         EXPORT_PARAMS="export CARGO_NET_GIT_FETCH_WITH_CLI=true; export CARGO_NET_RETRY=10; export CC=arm64-apple-darwin22.4-clang; export CXX=arm64-apple-darwin22.4-clang++;"    
+#         ADDITIONAL_PARAMS="CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=arm64-apple-darwin22.4-clang LIBZ_SYS_STATIC=0"
+#         STRIP_COMMAND="arm64-apple-darwin22.4-strip"
+#     fi
 
-    rm -rf ${PROJECTDIR}/entrusted_container/target
-    rm -rf ${PROJECTDIR}/entrusted_client/target
-    rm -rf ${PROJECTDIR}/entrusted_webclient/target
-    rm -rf ${PROJECTDIR}/entrusted_webserver/target
+#     mkdir -p ${ARTIFACTSDIR}
 
-    cd ${PROJECTDIR}
+#     rm -rf ${PROJECTDIR}/entrusted_container/target
+#     rm -rf ${PROJECTDIR}/entrusted_client/target
+#     rm -rf ${PROJECTDIR}/entrusted_webclient/target
+#     rm -rf ${PROJECTDIR}/entrusted_webserver/target
 
-    echo "Building all Mac OS binaries for ${CPU_ARCH}"
+#     cd ${PROJECTDIR}
 
-    podman run --rm \
-           --volume "${PROJECTDIR}":/root/src \
-           --workdir /root/src \
-           docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} \
-           sh -c "${EXPORT_PARAMS} ${BUILD_PREAMBLE}; ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --target  ${RUST_TARGET}  --manifest-path /root/src/entrusted_webserver/Cargo.toml && ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --features=gui --target ${RUST_TARGET} --manifest-path /root/src/entrusted_client/Cargo.toml && ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --target ${RUST_TARGET} --manifest-path /root/src/entrusted_webclient/Cargo.toml && ${STRIP_COMMAND} /root/src/entrusted_client/target/${RUST_TARGET}/release/entrusted-cli && ${STRIP_COMMAND} /root/src/entrusted_client/target/${RUST_TARGET}/release/entrusted-gui && ${STRIP_COMMAND} /root/src/entrusted_webclient/target/${RUST_TARGET}/release/entrusted-webclient && ${STRIP_COMMAND} /root/src/entrusted_webserver/target/${RUST_TARGET}/release/entrusted-webserver" || (sleep 10 && podman run --rm --volume "${PROJECTDIR}":/root/src --workdir /root/src docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} sh -c "${EXPORT_PARAMS} ${BUILD_PREAMBLE}; ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --target  ${RUST_TARGET}  --manifest-path /root/src/entrusted_webserver/Cargo.toml && ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --features=gui --target ${RUST_TARGET} --manifest-path /root/src/entrusted_client/Cargo.toml && ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --target ${RUST_TARGET} --manifest-path /root/src/entrusted_webclient/Cargo.toml && ${STRIP_COMMAND} /root/src/entrusted_client/target/${RUST_TARGET}/release/entrusted-cli && ${STRIP_COMMAND} /root/src/entrusted_client/target/${RUST_TARGET}/release/entrusted-gui && ${STRIP_COMMAND} /root/src/entrusted_webclient/target/${RUST_TARGET}/release/entrusted-webclient && ${STRIP_COMMAND} /root/src/entrusted_webserver/target/${RUST_TARGET}/release/entrusted-webserver")
+#     echo "Building all Mac OS binaries for ${CPU_ARCH}"
+
+#     podman run --rm \
+#            --volume "${PROJECTDIR}":/root/src \
+#            --workdir /root/src \
+#            docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} \
+#            sh -c "${EXPORT_PARAMS} ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --target  ${RUST_TARGET}  --manifest-path /root/src/entrusted_webserver/Cargo.toml && ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --features=gui --target ${RUST_TARGET} --manifest-path /root/src/entrusted_client/Cargo.toml && ${ADDITIONAL_PARAMS} ${RUSTFLAGS_PARAMS} cargo build --release --target ${RUST_TARGET} --manifest-path /root/src/entrusted_webclient/Cargo.toml && ${STRIP_COMMAND} /root/src/entrusted_client/target/${RUST_TARGET}/release/entrusted-cli && ${STRIP_COMMAND} /root/src/entrusted_client/target/${RUST_TARGET}/release/entrusted-gui && ${STRIP_COMMAND} /root/src/entrusted_webclient/target/${RUST_TARGET}/release/entrusted-webclient && ${STRIP_COMMAND} /root/src/entrusted_webserver/target/${RUST_TARGET}/release/entrusted-webserver"    
     
-    retVal=$?
-    if [ $retVal -ne 0 ]; then
-        echo "Failure"
-        exit 1
-    fi        
+#     retVal=$?
+#     if [ $retVal -ne 0 ]; then
+#         echo "Failure"
+#         exit 1
+#     fi        
     
-    cp ${PROJECTDIR}/entrusted_client/target/${RUST_TARGET}/release/entrusted-cli ${ARTIFACTSDIR}
-    cp ${PROJECTDIR}/entrusted_client/target/${RUST_TARGET}/release/entrusted-gui ${ARTIFACTSDIR}
-    cp ${PROJECTDIR}/entrusted_webclient/target/${RUST_TARGET}/release/entrusted-webclient ${ARTIFACTSDIR}
-    cp ${PROJECTDIR}/entrusted_webserver/target/${RUST_TARGET}/release/entrusted-webserver ${ARTIFACTSDIR}
+#     cp ${PROJECTDIR}/entrusted_client/target/${RUST_TARGET}/release/entrusted-cli ${ARTIFACTSDIR}
+#     cp ${PROJECTDIR}/entrusted_client/target/${RUST_TARGET}/release/entrusted-gui ${ARTIFACTSDIR}
+#     cp ${PROJECTDIR}/entrusted_webclient/target/${RUST_TARGET}/release/entrusted-webclient ${ARTIFACTSDIR}
+#     cp ${PROJECTDIR}/entrusted_webserver/target/${RUST_TARGET}/release/entrusted-webserver ${ARTIFACTSDIR}
 
-    # See https://github.com/zhlynn/zsign
-    # See https://forums.ivanti.com/s/article/Obtaining-an-Apple-Developer-ID-Certificate-for-macOS-Provisioning?language=en_US&ui-force-components-controllers-recordGlobalValueProvider.RecordGvp.getRecord=1
-    # echo "TODO need to create signed app bundle with proper entitlements, do we need to pay to share software for free too??????????????"
+#     # See https://github.com/zhlynn/zsign
+#     # See https://forums.ivanti.com/s/article/Obtaining-an-Apple-Developer-ID-Certificate-for-macOS-Provisioning?language=en_US&ui-force-components-controllers-recordGlobalValueProvider.RecordGvp.getRecord=1
+#     # echo "TODO need to create signed app bundle with proper entitlements, do we need to pay to share software for free too??????????????"
 
-    echo "Creating Entrusted appbundle"
-    cd ${SCRIPTDIR}
-    APPNAME=Entrusted
-    APPBUNDLE=${ARTIFACTSDIR}/${APPNAME}.app
-    APPDMGDIR=${ARTIFACTSDIR}/dmg
-    APPBUNDLECONTENTS=${APPBUNDLE}/Contents
-    APPBUNDLETMP=${APPBUNDLE}/tmp
-    APPBUNDLEEXE=${APPBUNDLECONTENTS}/MacOS
-    APPBUNDLERESOURCES=${APPBUNDLECONTENTS}/Resources
-    APPBUNDLECOMPANY="Rimero Solutions Inc"
-    APPBUNDLEVERSION=${APPVERSION}
+#     echo "Creating Entrusted appbundle"
+#     cd ${SCRIPTDIR}
+#     APPNAME=Entrusted
+#     APPBUNDLE=${ARTIFACTSDIR}/${APPNAME}.app
+#     APPDMGDIR=${ARTIFACTSDIR}/dmg
+#     APPBUNDLECONTENTS=${APPBUNDLE}/Contents
+#     APPBUNDLETMP=${APPBUNDLE}/tmp
+#     APPBUNDLEEXE=${APPBUNDLECONTENTS}/MacOS
+#     APPBUNDLERESOURCES=${APPBUNDLECONTENTS}/Resources
+#     APPBUNDLECOMPANY="Rimero Solutions Inc"
+#     APPBUNDLEVERSION=${APPVERSION}
 
-    mkdir -p ${APPDMGDIR}
-    mkdir -p ${APPBUNDLE}
-    mkdir -p ${APPBUNDLE}/Contents
-    mkdir -p ${APPBUNDLE}/Contents/MacOS
-    mkdir -p ${APPBUNDLE}/Contents/Resources
-    mkdir -p ${APPBUNDLETMP}    
+#     mkdir -p ${APPDMGDIR}
+#     mkdir -p ${APPBUNDLE}
+#     mkdir -p ${APPBUNDLE}/Contents
+#     mkdir -p ${APPBUNDLE}/Contents/MacOS
+#     mkdir -p ${APPBUNDLE}/Contents/Resources
+#     mkdir -p ${APPBUNDLETMP}    
 
-    podman run --rm -v "${PROJECTDIR}":/projectdir -v "${APPBUNDLETMP}":/app_bundle_dir docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} sh -c "convert /project_dir/images/${APPNAME}_icon.png /app_bundle_dir/${APPNAME}_icon.jpg && convert -scale 16x16 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_16_16.png && convert -scale 32x32 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg  /app_bundle_dir/${APPNAME}_32_32.png && convert -scale 128x128 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_128_128.png && convert -scale 256x256  -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg  /app_bundle_dir/${APPNAME}_256_256.png && convert -scale 512x512 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_512_512.png && png2icns ${APPBUNDLERESOURCES}/${APPNAME}.icns /app_bundle_dir/${APPNAME}_16_16.png /app_bundle_dir/${APPNAME}_32_32.png /app_bundle_dir/${APPNAME}_128_128.png /app_bundle_dir/${APPNAME}_256_256.png /app_bundle_dir/${APPNAME}_512_512.png" || (sleep 10 && podman run --rm -v "${PROJECTDIR}":/projectdir -v "${APPBUNDLETMP}":/app_bundle_dir docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} sh -c "convert /project_dir/images/${APPNAME}_icon.png /app_bundle_dir/${APPNAME}_icon.jpg && convert -scale 16x16 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_16_16.png && convert -scale 32x32 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg  /app_bundle_dir/${APPNAME}_32_32.png && convert -scale 128x128 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_128_128.png && convert -scale 256x256  -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg  /app_bundle_dir/${APPNAME}_256_256.png && convert -scale 512x512 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_512_512.png && png2icns ${APPBUNDLERESOURCES}/${APPNAME}.icns /app_bundle_dir/${APPNAME}_16_16.png /app_bundle_dir/${APPNAME}_32_32.png /app_bundle_dir/${APPNAME}_128_128.png /app_bundle_dir/${APPNAME}_256_256.png /app_bundle_dir/${APPNAME}_512_512.png")
+#     podman run --rm \
+#            -v "${PROJECTDIR}":/projectdir \
+#            -v "${APPBUNDLETMP}":/app_bundle_dir
+#            docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} \
+#            sh -c "convert /project_dir/images/${APPNAME}_icon.png /app_bundle_dir/${APPNAME}_icon.jpg && convert -scale 16x16 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_16_16.png && convert -scale 32x32 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg  /app_bundle_dir/${APPNAME}_32_32.png && convert -scale 128x128 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_128_128.png && convert -scale 256x256  -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg  /app_bundle_dir/${APPNAME}_256_256.png && convert -scale 512x512 -depth 8 /app_bundle_dir/${APPNAME}_icon.jpg /app_bundle_dir/${APPNAME}_512_512.png && png2icns ${APPBUNDLERESOURCES}/${APPNAME}.icns /app_bundle_dir/${APPNAME}_16_16.png /app_bundle_dir/${APPNAME}_32_32.png /app_bundle_dir/${APPNAME}_128_128.png /app_bundle_dir/${APPNAME}_256_256.png /app_bundle_dir/${APPNAME}_512_512.png"
 
-    rm -rf ${APPBUNDLETMP}
+#     rm -rf ${APPBUNDLETMP}
     
-    cp ${SCRIPTDIR}/macos/Info.plist ${APPBUNDLECONTENTS}/
-    cp ${SCRIPTDIR}/macos/PkgInfo ${APPBUNDLECONTENTS}/
+#     cp ${SCRIPTDIR}/macos/Info.plist ${APPBUNDLECONTENTS}/
+#     cp ${SCRIPTDIR}/macos/PkgInfo ${APPBUNDLECONTENTS}/
 
-    cp ${PROJECTDIR}/entrusted_client/target/${RUST_TARGET}/release/entrusted-cli ${APPBUNDLEEXE}/
-    mv ${ARTIFACTSDIR}/entrusted-gui ${APPBUNDLEEXE}/${APPNAME}
-    podman run --rm -v "${APPBUNDLECONTENTS}":/app_bundle_contents docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} sh -c "perl -pi -e 's/_COMPANY_NAME_/${APPBUNDLECOMPANY}/g' /app_bundle_contents/Info.plist && perl -pi -e 's/_APPVERSION_/${APPBUNDLEVERSION}/g' /app_bundle_contents/Info.plist" || (sleep 10 && podman run --rm -v "${APPBUNDLECONTENTS}":/app_bundle_contents docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} sh -c "perl -pi -e 's/_COMPANY_NAME_/${APPBUNDLECOMPANY}/g' /app_bundle_contents/Info.plist && perl -pi -e 's/_APPVERSION_/${APPBUNDLEVERSION}/g' /app_bundle_contents/Info.plist")
+#     cp ${PROJECTDIR}/entrusted_client/target/${RUST_TARGET}/release/entrusted-cli ${APPBUNDLEEXE}/
+#     mv ${ARTIFACTSDIR}/entrusted-gui ${APPBUNDLEEXE}/${APPNAME}
+#     podman run --rm \
+#            -v "${APPBUNDLECONTENTS}":/app_bundle_contents \
+#            docker.io/uycyjnzgntrn/rust-macos:${RUST_CI_VERSION} \
+#            sh -c "perl -pi -e 's/_COMPANY_NAME_/${APPBUNDLECOMPANY}/g' /app_bundle_contents/Info.plist && perl -pi -e 's/_APPVERSION_/${APPBUNDLEVERSION}/g' /app_bundle_contents/Info.plist"
 
-    cp -r ${APPBUNDLE} ${APPDMGDIR}/
-    ln -s /Applications ${APPDMGDIR}/
-    podman run --rm -v "${ARTIFACTSDIR}":/files docker.io/sporsh/create-dmg "Entrusted" /files/dmg/ /files/entrusted-${APPVERSION}-macos-${CPU_ARCH}.dmg || (sleep 10 && podman run --rm -v "${ARTIFACTSDIR}":/files docker.io/sporsh/create-dmg "Entrusted" /files/dmg/ /files/entrusted-${APPVERSION}-macos-${CPU_ARCH}.dmg)
-    rm -rf ${APPDMGDIR}
-    mv ${ARTIFACTSDIR}/*.dmg ${ARTIFACTSDIR}/../
+#     cp -r ${APPBUNDLE} ${APPDMGDIR}/
+#     ln -s /Applications ${APPDMGDIR}/
+#     podman run --rm -v "${ARTIFACTSDIR}":/files docker.io/sporsh/create-dmg "Entrusted" /files/dmg/ /files/entrusted-${APPVERSION}-macos-${CPU_ARCH}.dmg
+#     rm -rf ${APPDMGDIR}
+#     mv ${ARTIFACTSDIR}/*.dmg ${ARTIFACTSDIR}/../
 
-    cp ${SCRIPTDIR}/release_README.txt ${ARTIFACTSDIR}/README.txt
+#     cp ${SCRIPTDIR}/release_README.txt ${ARTIFACTSDIR}/README.txt
 
-    cd ${ARTIFACTSDIR}/.. && zip -r entrusted-${APPVERSION}-macos-${CPU_ARCH}.zip entrusted-${APPVERSION}-macos-${CPU_ARCH}
-done
+#     cd ${ARTIFACTSDIR}/.. && zip -r entrusted-${APPVERSION}-macos-${CPU_ARCH}.zip entrusted-${APPVERSION}-macos-${CPU_ARCH}
+# done
 
 cd ${SCRIPTDIR}
